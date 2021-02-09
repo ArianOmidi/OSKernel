c520ff9aa265d502bb2f10e6b4db9424bca514a9
Arian OMIDI <aomidi@teaching.CS.McGill.CA>
2021-02-09 13:20:14 -0500

    init commit

---
+++ src/c/kernel.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shell.h"
+
+int main(int argc, char* argv[]){
+	shellUI();
+}
---
+++ src/c/shell.c
@@ -0,0 +1,32 @@
+#include "interpreter.h"
+#include "shellmemory.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int shellUI()
+{
+    printf("Welcome to the Arian Omidi shell!\n"
+           "Version 1.0 Created January 2021\n");
+
+    shell_memory_initialize();
+
+    while (!feof(stdin))
+    {
+        printf("$ ");
+        fflush(stdout);
+
+        char *line = NULL;
+        size_t linecap = 0;
+        if (getline(&line, &linecap, stdin) == -1)
+            break;
+
+        (void)interpret(line);
+        free(line);
+    }
+
+    shell_memory_destory();
+
+    return 0;
+}
