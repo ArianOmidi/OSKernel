b9c77aa0951549e28751d934c94a7858c4fd5928
Arian Omidi <arian.omidi@icloud.com>
2021-03-11 18:38:50 -0500

    copyFile() working

--- src/c/memorymanager.c
+++ src/c/memorymanager.c
@@ -1,2 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int programNum = 0;
+
+FILE *copyFile(FILE *src, char *path) {
+  char programName[200];
+  sprintf(programName, "%s/program%d.txt", path, ++programNum);
+
+  FILE *copy = fopen(programName, "w");
+  if (copy == NULL) return NULL;
+
+  // Copy src contents
+  char c = fgetc(src);
+  while (c != EOF) {
+    fputc(c, copy);
+    c = fgetc(src);
+  }
+
+  fclose(src);
+  fclose(copy);
+
+  return copy;
+}
+
+int launcher(FILE *p) {
+  // Copy the entire file into the Backing Store
+  FILE *copy = copyFile(p, "BackingStore");
+  fclose(p);
+
+  if (copy == NULL) return -1;
+
+  return 0;
+}
+
+// -- HELPER METHODS -- //
+
+int countTotalPages(FILE *f) {}
---
+++ src/c/memorymanager.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int launcher(FILE *p);
+
+int copyFile(char *program, char *path);
+
+// int copyFile(FILE *src, char *path);
